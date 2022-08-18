864c7f86fd209e78b902c22276275160da38a254
Guilherme Pessoa <guilhermepessoa1993@gmail.com>
2022-08-18 00:37:32 -0300

    Lessons Learned

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,11 @@
-{
-  "reporter": "cypress-multi-reporters",
-  "reporterOptions": {
-    "configFile": "reporter.json"
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
   },
-  "video": false,
-  "screenshotOnRunFailure": false,
-  "viewportWidth": 1366,
-  "viewportHeight": 768,
-  "defaultCommandTimeout": 10000,
-  "pageLoadTimeout": 10000
-}
+})
