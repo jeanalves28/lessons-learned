203da273b5a44212d42869643fb06a95ab397638
Caio Mateus <dmateoinvestimentos@gmail.com>
2022-08-23 18:21:23 -0300

    auterado texto

---
+++ exercício aula js.js
@@ -0,0 +1,8 @@
+let Noel = 50;
+let Ellen = 30;
+let Nasc = 35
+let Pessoa=[]
+//Crie um algoritmo que recebe a idade de Noel, Ellen e Nasc e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
+
+for ()
